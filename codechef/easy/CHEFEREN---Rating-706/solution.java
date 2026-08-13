4851f87c73c3b03221a7453d62d5435f199cea4d
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-08-13 14:59:02 +0530

    ✅ CHEFEREN - Rating 706 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-13

---
+++ codechef/easy/CHEFEREN---Rating-706/solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Chef
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0)
+        {
+            int n = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(n==1)
+            {
+                System.out.println(b);
+            }
+            else
+            {
+                System.out.println(((n/2)*a)+((n/2)*b));   
+            }
+        }
+    }
+}
