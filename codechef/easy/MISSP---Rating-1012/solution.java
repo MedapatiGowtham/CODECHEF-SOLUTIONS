62fab24cb2a831071080b2ce456f32f318d55b14
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-08-18 21:46:30 +0530

    ✅ MISSP - Rating 1012 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-18

---
+++ codechef/easy/MISSP---Rating-1012/solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Chef
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0)
+        {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for(int i = 0;i < n;i++)
+            {
+                arr[i] = sc.nextInt();
+            }
+            int oddDoll = 0;
+            for(int i = 0;i < n;i++)
+            {
+                oddDoll = oddDoll^arr[i];
+            }
+            System.out.println(oddDoll);
+        }
+    }
+}
