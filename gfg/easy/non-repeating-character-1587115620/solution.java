dabd320b038d1a89dcd882bb6a11372e11781aae
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-08-18 11:18:40 +0530

    ✅ Non Repeating Character | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-17

--- gfg/easy/non-repeating-character-1587115620/solution.java
+++ gfg/easy/non-repeating-character-1587115620/solution.java
@@ -1,19 +1,17 @@
 class Solution {
     public char nonRepeatingChar(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
-
-        // Count frequency
-        for (char c : s.toCharArray()) {
+        for (char c : s.toCharArray())
+        {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
-
-        // Find first non-repeating character
-        for (char c : s.toCharArray()) {
-            if (map.get(c) == 1) {
+        for (char c : s.toCharArray())
+        {
+            if (map.get(c) == 1)
+            {
                 return c;
             }
         }
-
         return '$';
     }
 }
