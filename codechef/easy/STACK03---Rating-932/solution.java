487889ef25e58167eaf7075aaf2875c83e73af7a
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-05 22:33:15 +0530

    ✅ STACK03 - Rating 932 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-05

--- codechef/easy/STACK03---Rating-932/solution.java
+++ codechef/easy/STACK03---Rating-932/solution.java
@@ -1,10 +1,8 @@
 import java.util.Scanner;
-
 class Stack {
     private static final int STACK_CAPACITY = 101;
     private char[] stackArray = new char[STACK_CAPACITY];
     private int topIndex = -1;
-
     void push(char character) {
        if(isFull()) {
            System.out.println("Stack is full");
@@ -13,7 +11,6 @@ class Stack {
            stackArray[topIndex] = character;
        }
     }
-
     char pop() {
        if (isEmpty()) {
            System.out.println("Stack is empty");
@@ -24,35 +21,27 @@ class Stack {
            return element;
        }
     }
-
     boolean isEmpty() {
         return topIndex == -1;
     }
-
     boolean isFull() {
         return topIndex >= STACK_CAPACITY - 1;
     }
 }
-
 class ReverseString {
     public static void main(String[] args) {
         String inputString = "Hello, World!";
         int inputLength = inputString.length();
 
         Stack charStack = new Stack();
-
-        // Push each character onto the stack
         for (int i = 0; i < inputLength; i++) {
             char currentChar = inputString.charAt(i);
             charStack.push(currentChar);
         }
-
-        // Pop the characters from the stack to construct the reversed string
         StringBuilder reversedString = new StringBuilder();
         while (!charStack.isEmpty()) {
             reversedString.append(charStack.pop());
         }
-
         System.out.println(reversedString.toString());
     }
 }
