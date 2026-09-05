142a7f2e4458ab13e37ec93fccf47691a9bcd0c4
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-05 22:38:20 +0530

    ✅ STACK05 - Rating 932 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-05

---
+++ codechef/easy/STACK05---Rating-932/solution.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+class BalancedParentheses {
+    static final int MAX_SIZE = 101;
+    static char[] a = new char[MAX_SIZE];
+    static int top = -1;
+
+    static void push(char ele) {
+        if (top <= MAX_SIZE - 1) {
+            a[++top] = ele;
+        } else {
+            System.out.println("Stack is full. Cannot push: " + ele);
+        }
+    }
+
+    static char pop() {
+        if (top >= 0) {
+            char ele = a[top];
+            top--;
+            return ele;
+        } else {
+            System.out.println("Stack is empty. Cannot pop.");
+            return '-';
+        }
+    }
+
+    static boolean isEmpty() {
+        return top == -1;
+    }
+
+    static boolean isFull() {
+        return top >= MAX_SIZE;
+    }
+
+    static boolean isMatchingPair(char open, char close) {
+        return (open == '(' && close == ')');
+    }
+
+    static boolean isBalanced(String expression) {
+        for (int i=0; i<expression.length(); i++) {
+            char ch = expression.charAt(i);
+            if(ch == '(') {
+                push(ch);
+            } else if(ch == ')') {
+                if(isEmpty()) {
+                    return false;
+                }
+                pop();
+            }
+        }
+        return isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            String str = scanner.next();
+            System.out.println(str + " : " + isBalanced(str));
+        }
+    }
+}
