5796aff25a6c6f937a170be72ea86e76d74406b3
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-06 23:29:33 +0530

    ✅ LJAAS40 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-06

--- codechef/medium/LJAAS40/solution.java
+++ codechef/medium/LJAAS40/solution.java
@@ -1,8 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
-        String one = "Coding";
-        String two = "Chaf";
-        two = two.replace('a', 'e');
-        System.out.println(one + " " + two);
-    }
+ class Codechef
+{
+	public static void main (String[] args) 
+	{
+		String one = "Coding";
+        String two = "on";
+        String three = "CodeChef";
+        String four = one+" "+two+" "+three;
+        System.out.println(one+" - "+one.length());
+        System.out.println(two+" - "+two.length());
+        System.out.println(three+" - "+three.length());
+        System.out.println(four+" - "+four.length());
+        
+        
+        // Complete the code
+        
+
+	}
 }
