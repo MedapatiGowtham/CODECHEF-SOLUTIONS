9bce59056447d6504d0dc3d30835117536a450fa
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-06 23:30:42 +0530

    ✅ LJAAS50 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-06

---
+++ codechef/medium/LJAAS50/solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		String one = sc.nextLine();
+		String two = sc.nextLine();
+		System.out.println(one+" "+two);
+
+	}
+}
