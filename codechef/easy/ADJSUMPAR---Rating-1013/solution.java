6c3e27e658432bead9d275e97178028bebf727ce
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-07 23:26:28 +0530

    ✅ ADJSUMPAR - Rating 1013 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-07

---
+++ codechef/easy/ADJSUMPAR---Rating-1013/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            int count = 0;
+
+            for (int i = 0; i < N; i++) {
+                count += sc.nextInt();
+            }
+
+            System.out.println(count % 2 == 0 ? "YES" : "NO");
+        }
+    }
+}
