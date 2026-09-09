5458af718a7436e420ba464bda81beb7515f7cb8
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-09 20:29:52 +0530

    ✅ UNQSHF | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

---
+++ codechef/medium/UNQSHF/solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int t = sc.nextInt();
+        while(t--> 0) {
+            int n = sc.nextInt();
+            String 
+        }
+    }
+}
