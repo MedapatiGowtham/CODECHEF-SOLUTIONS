728a91d272388b846e4d47b4a21ffe2d40876648
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-09 20:07:19 +0530

    ✅ EGGBUY | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

---
+++ codechef/medium/EGGBUY/solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        if((x*12+z) > (y*12+z)) {
+            System.out.println(x*12);
+        }
+    }
+}
