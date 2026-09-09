bdefc71dd2315151a333d3086582cb04aea9dc0c
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-09 20:35:11 +0530

    ✅ UNQSHF | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/UNQSHF/solution.java
+++ codechef/medium/UNQSHF/solution.java
@@ -2,11 +2,26 @@ import java.util.*;
 class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        HashMap<Integer, Integer> hm = new HashMap<>();
         int t = sc.nextInt();
         while(t--> 0) {
             int n = sc.nextInt();
-            String 
+            String a = sc.next();
+            String b = sc.next();
+            int countOfa = 0;
+            int countOfb = 0;
+            for(int i=0; i<a.length(); i++) {
+                if(a.charAt(i) == 'a') {
+                    countOfa++;
+                }
+                if(b.charAt(i) == 'a') {
+                    countOfb++;
+                }
+            }
+            if(countOfa+countOfb == n) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
         }
     }
 }
