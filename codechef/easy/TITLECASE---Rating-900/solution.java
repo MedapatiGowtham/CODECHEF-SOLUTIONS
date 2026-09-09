7096e446a1a9a174be4b5499f228dabf6d65b502
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-09 22:22:32 +0530

    ✅ TITLECASE - Rating 900 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/easy/TITLECASE---Rating-900/solution.java
+++ codechef/easy/TITLECASE---Rating-900/solution.java
@@ -1,26 +1,30 @@
 import java.util.*;
-
 public class Main {
+    public static String titleCase(String s) {
+        String[] words = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            boolean isAcronym = word.equals(word.toUpperCase());
+
+            if (!isAcronym) {
+                word = word.substring(0, 1).toUpperCase()
+                     + word.substring(1).toLowerCase();
+            }
+            if (i > 0) {
+                result.append(" ");
+            }
+            result.append(word);
+        }
+        return result.toString();
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int T = sc.nextInt();
-        sc.nextLine();
-
+        sc.nextLine(); 
         while (T-- > 0) {
             String s = sc.nextLine();
-            String[] words = s.split(" ");
-
-            for (int i = 0; i < words.length; i++) {
-                String word = words[i];
-
-                if (!word.equals(word.toUpperCase())) {
-                    words[i] = Character.toUpperCase(word.charAt(0))
-                            + word.substring(1).toLowerCase();
-                }
-            }
-
-            System.out.println(String.join(" ", words));
+            System.out.println(titleCase(s));
         }
     }
 }
