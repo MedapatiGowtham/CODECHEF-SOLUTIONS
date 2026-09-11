b62588ca4ad6dca91f03327f537e897e09e26323
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-11 22:07:42 +0530

    ✅ WCC - Rating 935 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-11

--- codechef/easy/WCC---Rating-935/solution.java
+++ codechef/easy/WCC---Rating-935/solution.java
@@ -1,31 +1,47 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        
+        // Read the number of test cases
+        if (!scanner.hasNextInt()) return;
+        int t = scanner.nextInt();
+
         while (t-- > 0) {
-            int x = sc.nextInt();
-            String s = sc.next();
-            int carlsen = 0;
-            int chef = 0;
-            for (char c : s.toCharArray()) {
-                if (c == 'C') {
-                    carlsen += 2;
-                } else if (c == 'N') {
-                    chef += 2;
+            // Read X (prize pool multiplier)
+            int x = scanner.nextInt();
+            
+            // Read the match results string
+            String s = scanner.next();
+            
+            int carlsenPoints = 0;
+            int chefPoints = 0;
+            
+            // Calculate points for each game
+            for (int i = 0; i < s.length(); i++) {
+                char outcome = s.charAt(i);
+                if (outcome == 'C') {
+                    carlsenPoints += 2;
+                } else if (outcome == 'N') {
+                    chefPoints += 2;
                 } else {
-                    carlsen++;
-                    chef++;
+                    // Draw: both get 1 point
+                    carlsenPoints += 1;
+                    chefPoints += 1;
                 }
             }
-            if (carlsen > chef) {
+            
+            // Determine the prize money based on points
+            if (carlsenPoints > chefPoints) {
                 System.out.println(60 * x);
-            } else if (carlsen == chef) {
+            } else if (carlsenPoints == chefPoints) {
                 System.out.println(55 * x);
             } else {
                 System.out.println(40 * x);
             }
         }
+        
+        scanner.close();
     }
 }
