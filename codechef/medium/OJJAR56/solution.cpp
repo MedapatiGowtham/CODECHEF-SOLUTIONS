041e7a5da0ed372af2ab60077503ff0b4ed27453
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-15 11:09:05 +0530

    ✅ OJJAR56 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-15

---
+++ codechef/medium/OJJAR56/solution.cpp
@@ -0,0 +1,21 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        // add the width here 
+        // add the height here 
+
+        backgroundColor: 'lightblue',
+        color: 'white',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '18px'
+      }}
+    >
+      Styled Box
+    </div>
+  );
+}
+        width: '200px',
+        height: '100px',
