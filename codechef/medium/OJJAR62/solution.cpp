8fb712dbd74d5ce1c8e23dbbb10b4c1b9e0f1821
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-15 11:19:50 +0530

    ✅ OJJAR62 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-15

---
+++ codechef/medium/OJJAR62/solution.cpp
@@ -0,0 +1,25 @@
+function Welcome() {
+  return <h2>Welcome to My React App! 🎉</h2>;
+}
+
+// Creating the new Goodbye component
+function Goodbye() {
+
+
+
+function App() {
+  return (
+    <div>
+      <Welcome /> {/* Using the Welcome component */}
+      <p>React components make the UI more structured and reusable.</p>
+      {/* Using the Goodbye component */}
+    
+    
+    </div>
+  );
+}
+      <Goodbye/>
+  return <h3>Thanks for visiting! see you soon!</h3>
+}
+
+export default App;
