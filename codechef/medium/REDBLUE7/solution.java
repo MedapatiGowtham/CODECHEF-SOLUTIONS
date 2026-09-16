b0e1155afc45c943fa235b438a55ea1336e0a60d
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-16 20:55:39 +0530

    ✅ REDBLUE7 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

---
+++ codechef/medium/REDBLUE7/solution.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int 
+        }
+    }
+}
