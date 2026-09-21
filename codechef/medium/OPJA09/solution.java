792e99a4d7bf3b605015b09a1c1117acd532eaa0
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-21 23:36:25 +0530

    ✅ OPJA09 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ codechef/medium/OPJA09/solution.java
@@ -0,0 +1,30 @@
+class Student {
+    public String name;
+    public int score;
+    public int age;
+
+    public void eligible() {
+        // Update your code below this line
+        if(score>10 && age>20)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+        
+    }
+}
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Student obj = new Student();
+        obj.name = "Tom";
+        obj.score = 15;
+        obj.age = 21;
+        obj.eligible();
+	}
+}
