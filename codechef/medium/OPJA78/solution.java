0434a65d8149abfc194ea27cec44b1c669dc8c1c
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-21 23:38:07 +0530

    ✅ OPJA78 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ codechef/medium/OPJA78/solution.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class BankAccount {
+    public static int totalBalance;
+    public int balance;
+
+    public BankAccount(int amount) {
+        totalBalance += amount;
+    }
+}
+
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int amount = scanner.nextInt();
+        BankAccount account1 = new BankAccount(amount);
+
+        amount = scanner.nextInt();
+        BankAccount account2 = new BankAccount(amount);
+
+        System.out.println(BankAccount.totalBalance);
+
+        scanner.close();
+    }
+}
