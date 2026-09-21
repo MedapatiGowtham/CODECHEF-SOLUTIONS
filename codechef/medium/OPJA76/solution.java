6bed0674c786c99433f29b21b6e5b2af8cea7668
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-21 23:39:50 +0530

    ✅ OPJA76 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/OPJA76/solution.java
+++ codechef/medium/OPJA76/solution.java
@@ -1,23 +1,30 @@
-class Car {
-    public String carName;
+import java.util.Scanner;
 
-    // Parameterized constructor
-    public Car(String carName) {
-        this.carName = carName;
+class Student {
+    public String name;
+    public int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
-    // Copy constructor (constructor that takes an object of the same class)
-    public Car(Car c) {
-        this.carName = c.carName;
+    public void display() {
+        System.out.println(name);
+        System.out.println(age);
     }
 }
 
 class Codechef {
     public static void main(String[] args) {
-        Car originalCar = new Car("Beat"); // Parameterized constructor called here
-        System.out.println(originalCar.carName);
+        Scanner scanner = new Scanner(System.in);
 
-        Car copiedCar = new Car(originalCar); // Copy constructor called here
-        System.out.println(copiedCar.carName);
+        String name = scanner.next();
+        int age = scanner.nextInt();
+
+        Student obj = new Student(name, age);
+        obj.display();
+
+        scanner.close();
     }
 }
