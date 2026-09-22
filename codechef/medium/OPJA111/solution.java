8102a499eab7a9c6529ddb84469a06ed25ba8a2f
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-22 22:53:17 +0530

    ✅ OPJA111 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-22

---
+++ codechef/medium/OPJA111/solution.java
@@ -0,0 +1,31 @@
+class Calculator {
+    // Method to add two integers
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method to add three integers
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Method to add four integers
+    int add(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+}
+
+class Codechef {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        // Using the different overloaded add methods
+        int sum1 = calculator.add(2, 3);
+        int sum2 = calculator.add(2, 3, 4);
+        int sum3 = calculator.add(2, 3, 4, 5);
+
+        System.out.println("Sum1: " + sum1);
+        System.out.println("Sum2: " + sum2);
+        System.out.println("Sum3: " + sum3);
+    }
+}
