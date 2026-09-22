5785472cceff435444d59f9cb20514d764de254c
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-22 22:53:46 +0530

    ✅ OPJA109 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-22

---
+++ codechef/medium/OPJA109/solution.cpp
@@ -0,0 +1,21 @@
+class Calculator {
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+}
+
+class Codechef{
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        int intDifference = calculator.subtract(10, 4); 
+        double doubleDifference = calculator.subtract(5.5, 2.0); 
+
+        System.out.println("Integer Difference: " + intDifference);
+        System.out.println("Double Difference: " + doubleDifference);
+    }
+}
