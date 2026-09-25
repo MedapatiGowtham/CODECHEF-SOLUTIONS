1883aa84921b092f04a72e9bd9d78ff3cb2abc09
Gowtham <medapatigowtham9@gmail.com>
2026-09-25 23:18:54 +0530

    ✅ OPJA138 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-25

--- codechef/medium/OPJA138/solution.java
+++ codechef/medium/OPJA138/solution.java
@@ -1,26 +1,29 @@
-class Complex {
-    public double real;
-    public double imaginary;
+class Name {
+    public String name; // Data member to store a name
 
-    public Complex(double r, double i) {
-        real = r;
-        imaginary = i;
-    }
-
-    public Complex add(Complex other) {
-        double resultReal = real + other.real;
-        double resultImaginary = imaginary + other.imaginary;
-        return new Complex(resultReal, resultImaginary);
+    // Static method to concatenate two Name objects and return a new Name object
+    public static Name concatenateNames(Name obj1, Name obj2) {
+        // Create a new Name object and set its name data member to the concatenated result
+        Name result = new Name();
+        result.name = obj1.name + " " + obj2.name;
+        return result;
     }
 }
 
 class Codechef {
     public static void main(String[] args) {
-        Complex num1 = new Complex(2.5, 3.0);
-        Complex num2 = new Complex(1.0, -1.5);
+        // Create an instance of the Name class and set the name data member
+        Name name1 = new Name();
+        name1.name = "Tom";
 
-        Complex result = num1.add(num2);
+        // Create another instance of the Name class and set the name data member
+        Name name2 = new Name();
+        name2.name = "Jerry";
 
-        System.out.println("Result: " + result.real + " + " + result.imaginary + "i");
+        // Use the concatenateNames static method to concatenate the names
+        Name concatenatedName = Name.concatenateNames(name1, name2);
+
+        // Display the concatenated name using the name data member of the result
+        System.out.println(concatenatedName.name);
     }
 }
